2ab1a0c1cc9a9a73ecc72b78a4e3173995fd0e1e
Shreyash Dhulrao <shreyashdhularao@gmail.com>
2024-06-07 13:12:03 +0530

    first update (navbar)

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import Navbar from './Components/Frontend/Navbar'
+import Main from './Components/Frontend/Main'
 
 function App() {
+  const [Light, setLight] = useState('zinc-50')
+  const [Light1, setLight1] = useState('zinc-200')
+  const [Text, setText] = useState('neutral-950')
+  const [TextD, setTextD] = useState('Dark')
+  const toggleButton =()=>{
+    if(Light === 'zinc-50'){
+      setLight('zinc-800')
+      setText('neutral-50')
+      setLight1('zinc-700')
+      setTextD('Light')
+    }
+    else{
+      setLight('zinc-50')
+      setText('neutral-950')
+      setLight1('zinc-200')
+      setTextD('Dark')
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar toggleButton={toggleButton} Background={Light} Text={Text} Text1={TextD} Light1={Light1} />
+      <Main />
     </div>
   );
 }
---
+++ src/Components/Frontend/Main.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Image1 from '../Images/Layer 1.jpg'
+
+const Main = () => {
+  return (
+    <div>
+        <img src={Image1} alt="" />
+    </div>
+  )
+}
+
+export default Main
